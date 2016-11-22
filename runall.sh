99fff1581d213be0d51a8aaf1583ea79a02ab3b2
Benedikt Geißler <benedikt@g5r.eu>
2016-11-22 20:39:52 +0100

    Apply hints by shellcheck
    Especially https://github.com/koalaman/shellcheck/wiki/SC2013

--- runall.sh
+++ runall.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-for account in `grep entry accounts.xml | awk -F '"' '{print $2}'`; do
-  server=$(echo $account | awk -F '@' '{print $2}')
+grep entry accounts.xml | awk -F '"' '{print $2}' | while IFS= read -r account
+do
+  server=$(echo "$account" | awk -F '@' '{print $2}')
   echo "testing $server"
-  java -jar target/ComplianceTester-0.1.jar $account > reports/$server.txt
-done;
+  java -jar target/ComplianceTester-0.1.jar "$account" > "reports/$server.txt"
+done
